85533df0f1adf26d563dc363c6d55ced23b7e544
dkbgroupcompany <139337270+dkbgroupcompany@users.noreply.github.com>
2023-11-08 00:10:33 -0800

    Add files via upload

--- transaction.js
+++ transaction.js
@@ -1,38 +1,6 @@
-const Transaction = require('../model/Transactions'); 
-const axios = require('axios')
+const express = require("express");
+const router = express.Router();
+const transactionHistory = require("../controllers/transactionHistory");
 
-async function transactiondate() {
-  try {
-    const response = await axios.get('http://worldtimeapi.org/api/timezone/Africa/Lagos');
-    let time = response.data.datetime;
-    time = time.split('.')[0];
-     time = time.replace('T', ' ');
-    return time;
-  } catch (error) {
-    console.error(error);
-  }
-}
-transactiondate();
-const handletransaction = (arrangeDate, userid, referenceid, amounts, walletBalance, types, recipients, descriptions, statuses, transactionDate, value) =>{
-const newTransaction = new Transaction({
-  arrangeDate:arrangeDate,
-  user: userid,
-  amount: amounts,
-  description: descriptions,
-  wallet:walletBalance,
-  refid:referenceid,
-  recipient:recipients,
-  type:types,
-  value:value,
-  status:statuses,
-  transactionDate:transactionDate
-});
-
-newTransaction.save()
-  
-
-
-}
-
-
-module.exports = handletransaction;
+router.get("/", transactionHistory);
+module.exports = router;
