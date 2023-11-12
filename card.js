65d9dcba0797f4b84aff8bc448a6b2d4abb58f85
dkbgroupcompany <139337270+dkbgroupcompany@users.noreply.github.com>
2023-11-12 10:12:56 -0800

    Update card.js

--- card.js
+++ card.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
-const savePin = require("savePin");
-const sellingcardPin = require('cardSelling')
+const savePin = require("./savePin");
+const sellingcardPin = require('./cardSelling')
 
 router.post("/save", savePin);
 router.post("/sell", sellingcardPin)
