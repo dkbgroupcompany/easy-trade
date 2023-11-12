ca2e608a5525b4aff6a864401c2160a6b288268a
dkbgroupcompany <139337270+dkbgroupcompany@users.noreply.github.com>
2023-11-12 10:04:01 -0800

    Update databundle.js

--- databundle.js
+++ databundle.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const datebundle = require("../controllers/databundle");
+const datebundle = require("databundle");
 
 router.post("/airtime", datebundle.airtimeForAllNewtwork);
 
